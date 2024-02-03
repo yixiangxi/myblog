50ee766731855f6613152fe4316ebf4500c828da
yixiangxi <114321546+yixiangxi@users.noreply.github.com>
2024-02-03 16:31:13 +0800

    开发后台用户修改，查询，删除接口

--- springboot/src/main/java/com/example/mapper/UserMapper.java
+++ springboot/src/main/java/com/example/mapper/UserMapper.java
@@ -4,6 +4,7 @@ import com.example.entity.Admin;
 import com.example.entity.User;
 
 import javax.annotation.Resource;
+import java.util.List;
 
 public interface UserMapper {
     /**
@@ -14,5 +15,11 @@ public interface UserMapper {
     User selectByUsername(String username);
 
 
+   int deleteById(Integer id);
 
+    int updateById(User user);
+
+    User selectById(Integer id);
+
+    List<User> selectAll(User user);
 }
--- springboot/src/main/java/com/example/service/UserService.java
+++ springboot/src/main/java/com/example/service/UserService.java
@@ -10,13 +10,18 @@ import com.example.common.Constants;
 import com.example.common.Result;
 import com.example.common.enums.ResultCodeEnum;
 import com.example.common.enums.RoleEnum;
+import com.example.entity.Admin;
 import com.example.entity.User;
 import com.example.exception.CustomException;
 import com.example.mapper.AdminMapper;
 import com.example.mapper.UserMapper;
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
+import java.util.List;
+
 @Service
 public class UserService {
     @Resource
@@ -43,4 +48,30 @@ public class UserService {
 
     }
 
+    public void deleteById(Integer id) {userMapper.deleteById(id);
+    }
+
+    public void deleteBatch(List<Integer> ids) {
+        for(Integer id:ids){
+            userMapper.deleteById(id);
+        }
+    }
+
+    public void updateById(User user) {
+        userMapper.updateById(user);
+    }
+
+    public User selectById(Integer id) {
+        return userMapper.selectById(id);
+    }
+
+    public List<User> selectAll(User user) {
+        return userMapper.selectAll(user);
+    }
+
+    public PageInfo<User> selectPage(User user, Integer pageNum, Integer pageSize) {
+        PageHelper.startPage(pageNum,pageSize);
+        List<User> userList = userMapper.selectAll(user);
+        return PageInfo.of(userList);
+    }
 }
